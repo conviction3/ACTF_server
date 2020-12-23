78cf6184bd44b570e58e81fa1ca18464bf06a21e
Bian <374044291@qq.com>
2020-12-24 05:33:26 +0800

    * change all comments from Chinese to English*

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
-import socket  # 导入 socket 模块
+import socket
 from app.utils import *
 
 client_num = 2
 
 if __name__ == '__main__':
-    s = socket.socket()  # 创建 socket 对象
-    host = socket.gethostname()  # 获取本地主机名
-    port = 12345  # 设置端口
-    s.bind((host, port))  # 绑定端口
+    # create socket instance
+    s = socket.socket()
+    # local host name
+    host = socket.gethostname()
+    # set local port
+    port = 12345
+    # bind local port to local host
+    s.bind((host, port))
 
-    s.listen(5)  # 等待客户端连接
+    s.listen(5)
     print("waiting for client")
 
     client_count = 0
     data_list = []
 
     while True:
-        c, addr = s.accept()  # 建立客户端连接
+        # establish connect to the client
+        c, addr = s.accept()
         data = c.recv(1024)
-        print("客户端{}的计算结果为{}".format(addr, data))
+        print("The calculation result of client {} is {}".format(addr, data))
         data_list.append(bytes2int(data))
 
         c.send(bytes('OK', encoding='utf-8'))
-        c.close()  # 关闭连接
+        # close socket
+        c.close()
 
         client_count += 1
         if client_count == client_num:
-            print("所有客户端均处理完成")
+            print("All the clients process done")
             break
 
     sum = sum(data_list)
-    print("最终计算结果：{}".format(sum))
+    print("Final result is: {}".format(sum))
